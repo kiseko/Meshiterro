338349004428efb3766669cb4adcec45011a8670
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-12 20:16:05 +0000

    [Start]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0db850568cf10c2db7abf6fdc3315fdac2ff997c8324a00f95c58e54b5aa36d7ee6d1b080129b102a2706abfd097641d018e802b0c4a91831598584d3ef2f03e'
